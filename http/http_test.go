da6b1e080f0ea914ed0e5ee15f06906c9bef77f4
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2021-03-07 00:32:57 -0600

    http.Agent() tests and fake implementation
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- http/http_test.go
+++ http/http_test.go
@@ -17,7 +17,10 @@ limitations under the License.
 package http_test
 
 import (
+	"bytes"
+	"errors"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -25,6 +28,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 	khttp "sigs.k8s.io/release-utils/http"
+	"sigs.k8s.io/release-utils/http/httpfakes"
 )
 
 func TestGetURLResponseSuccess(t *testing.T) {
@@ -75,4 +79,94 @@ func TestGetURLResponseFailedStatus(t *testing.T) {
 
 	// Then
 	require.NotNil(t, err)
+}
+
+func NewTestAgent() *khttp.Agent {
+	agent := khttp.NewAgent()
+	agent.SetImplementation(&httpfakes.FakeAgentImplementation{})
+	return agent
+}
+
+func TestAgentPost(t *testing.T) {
+	agent := NewTestAgent()
+
+	resp := &http.Response{
+		Status:        "200 OK",
+		StatusCode:    200,
+		Body:          ioutil.NopCloser(bytes.NewReader([]byte("hello sig-release!"))),
+		ContentLength: 18,
+		Close:         true,
+		Request:       &http.Request{},
+	}
+	defer resp.Body.Close()
+
+	// First simulate a successful request
+	fake := &httpfakes.FakeAgentImplementation{}
+	fake.SendPostRequestReturns(resp, nil)
+
+	agent.SetImplementation(fake)
+	body, err := agent.Post("http://www.example.com/", []byte("Test string"))
+	require.Nil(t, err)
+	require.Equal(t, body, []byte("hello sig-release!"))
+
+	// Now check error is handled
+	fake.SendPostRequestReturns(resp, errors.New("HTTP Post error"))
+	agent.SetImplementation(fake)
+	_, err = agent.Post("http://www.example.com/", []byte("Test string"))
+	require.NotNil(t, err)
+}
+
+func TestAgentGet(t *testing.T) {
+	agent := NewTestAgent()
+
+	resp := &http.Response{
+		Status:        "200 OK",
+		StatusCode:    200,
+		Body:          ioutil.NopCloser(bytes.NewReader([]byte("hello sig-release!"))),
+		ContentLength: 18,
+		Close:         true,
+		Request:       &http.Request{},
+	}
+	defer resp.Body.Close()
+
+	// First simulate a successful request
+	fake := &httpfakes.FakeAgentImplementation{}
+	fake.SendGetRequestReturns(resp, nil)
+
+	agent.SetImplementation(fake)
+	b, err := agent.Get("http://www.example.com/")
+	require.Nil(t, err)
+	require.Equal(t, b, []byte("hello sig-release!"))
+
+	// Now check error is handled
+	fake.SendGetRequestReturns(resp, errors.New("HTTP Post error"))
+	agent.SetImplementation(fake)
+	_, err = agent.Get("http://www.example.com/")
+	require.NotNil(t, err)
+}
+
+func TestAgentOptions(t *testing.T) {
+	agent := NewTestAgent()
+	fake := &httpfakes.FakeAgentImplementation{}
+	resp := &http.Response{
+		Status:        "Fake not found",
+		StatusCode:    404,
+		Body:          ioutil.NopCloser(bytes.NewReader([]byte("hello sig-release!"))),
+		ContentLength: 18,
+		Close:         true,
+		Request:       &http.Request{},
+	}
+	defer resp.Body.Close()
+
+	fake.SendGetRequestReturns(resp, nil)
+	agent.SetImplementation(fake)
+
+	// Test FailOnHTTPError
+	// First we fail on server errors
+	_, err := agent.WithFailOnHTTPError(true).Get("http://example.com/")
+	require.NotNil(t, err)
+
+	// Then we just note them and do not fail
+	_, err = agent.WithFailOnHTTPError(false).Get("http://example.com/")
+	require.Nil(t, err)
 }
