ec7946314f37466289383b0c3eeea933891b0a8a
cpanato <ctadeu@gmail.com>
2023-10-27 10:11:32 +0200

    update verifygomod mage function
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- mage/golangci-lint.go
+++ mage/golangci-lint.go
@@ -188,7 +188,7 @@ func testGo(verbose bool, tags string, pkgs ...string) error {
 
 // VerifyGoMod runs `go mod tidy` and `git diff --exit-code go.*` to ensure
 // all module updates have been checked in.
-func VerifyGoMod(scriptDir string) error {
+func VerifyGoMod() error {
 	minGoVersion := env.Default("MIN_GO_VERSION", defaultMinGoVersion)
 	if err := shx.RunV(
 		"go", "mod", "tidy", fmt.Sprintf("-compat=%s", minGoVersion),
