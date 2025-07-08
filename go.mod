f933c8edbe71e473c5c7644cd77722a8479f07f0
Carlos Panato <ctadeu@gmail.com>
2025-07-08 14:49:37 +0200

    add wrapper for tablewriter
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/maxbrunsfeld/counterfeiter/v6 v6.11.3
 	github.com/moby/term v0.5.2
 	github.com/nozzle/throttler v0.0.0-20180817012639-2ea982251481
+	github.com/olekukonko/tablewriter v1.0.8
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.9.1
 	github.com/stretchr/testify v1.10.0
@@ -20,10 +21,17 @@ require (
 	github.com/Azure/go-ansiterm v0.0.0-20250102033503-faa5f7b0171c // indirect
 	github.com/Masterminds/semver/v3 v3.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.15.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/magefile/mage v1.15.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/olekukonko/errors v0.0.0-20250405072817-4e6d85265da6 // indirect
+	github.com/olekukonko/ll v0.0.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	golang.org/x/mod v0.26.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
---
+++ util/tablewriter_test.go
@@ -0,0 +1,156 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/stretchr/testify/require"
+)
+
+// compareGolden compares actual output with golden file.
+func compareGolden(t *testing.T, actual, goldenFile string) {
+	t.Helper()
+
+	goldenPath := filepath.Join("testdata", goldenFile)
+	expected, err := os.ReadFile(goldenPath)
+	require.NoError(t, err, "Failed to read golden file: %s", goldenPath)
+
+	require.Equal(t, string(expected), actual, "Output doesn't match golden file: %s", goldenFile)
+}
+
+func TestNewTableWriter(t *testing.T) {
+	t.Parallel()
+
+	t.Run("NoOptions", func(t *testing.T) {
+		t.Parallel()
+
+		var output bytes.Buffer
+		table := NewTableWriter(&output)
+
+		require.NotNil(t, table)
+		require.IsType(t, &tablewriter.Table{}, table)
+
+		table.Header("Name", "Age")
+		_ = table.Append([]string{"John", "30"})
+		_ = table.Render()
+
+		compareGolden(t, output.String(), "no_options.golden")
+	})
+
+	t.Run("WithSingleOption", func(t *testing.T) {
+		t.Parallel()
+
+		var output bytes.Buffer
+		table := NewTableWriter(&output, tablewriter.WithMaxWidth(80))
+
+		require.NotNil(t, table)
+		require.IsType(t, &tablewriter.Table{}, table)
+
+		table.Header("Name", "Age")
+		_ = table.Append([]string{"John", "30"})
+		_ = table.Render()
+
+		compareGolden(t, output.String(), "with_single_option.golden")
+	})
+
+	t.Run("WithMultipleOptions", func(t *testing.T) {
+		t.Parallel()
+
+		var output bytes.Buffer
+		table := NewTableWriter(&output,
+			tablewriter.WithHeader([]string{"Name", "Age"}),
+			tablewriter.WithMaxWidth(80),
+		)
+
+		require.NotNil(t, table)
+		require.IsType(t, &tablewriter.Table{}, table)
+
+		_ = table.Append([]string{"John", "30"})
+		_ = table.Render()
+
+		compareGolden(t, output.String(), "with_multiple_options.golden")
+	})
+
+	t.Run("WithHeaderOption", func(t *testing.T) {
+		t.Parallel()
+
+		var output bytes.Buffer
+		table := NewTableWriter(&output, tablewriter.WithHeader([]string{"Name", "Age"}))
+
+		require.NotNil(t, table)
+		require.IsType(t, &tablewriter.Table{}, table)
+
+		_ = table.Append([]string{"John", "30"})
+		_ = table.Render()
+
+		compareGolden(t, output.String(), "with_header_option.golden")
+	})
+
+	t.Run("WithFooterOption", func(t *testing.T) {
+		t.Parallel()
+
+		var output bytes.Buffer
+		table := NewTableWriter(&output, tablewriter.WithFooter([]string{"Total", "1"}))
+
+		require.NotNil(t, table)
+		require.IsType(t, &tablewriter.Table{}, table)
+
+		table.Header("Name", "Age")
+		_ = table.Append([]string{"John", "30"})
+		_ = table.Render()
+
+		compareGolden(t, output.String(), "with_footer_option.golden")
+	})
+
+	t.Run("EmptyTable", func(t *testing.T) {
+		t.Parallel()
+
+		var output bytes.Buffer
+		table := NewTableWriter(&output)
+
+		require.NotNil(t, table)
+		require.IsType(t, &tablewriter.Table{}, table)
+
+		table.Header("Name", "Age")
+		_ = table.Render()
+
+		compareGolden(t, output.String(), "empty_table.golden")
+	})
+
+	t.Run("MultipleRows", func(t *testing.T) {
+		t.Parallel()
+
+		var output bytes.Buffer
+		table := NewTableWriter(&output)
+
+		require.NotNil(t, table)
+		require.IsType(t, &tablewriter.Table{}, table)
+
+		table.Header("Name", "Age", "City")
+		_ = table.Append([]string{"John", "30", "New York"})
+		_ = table.Append([]string{"Jane", "25", "Boston"})
+		_ = table.Append([]string{"Bob", "35", "Chicago"})
+		_ = table.Render()
+
+		compareGolden(t, output.String(), "multiple_rows.golden")
+	})
+}
