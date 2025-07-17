9eb8ae7fa84f91cd482b214c78153fc397cd3d2d
Carlos Panato <ctadeu@gmail.com>
2025-07-17 15:19:06 +0200

    add more wrapper fiunc for tablewriter
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- util/tablewriter.go
+++ util/tablewriter.go
@@ -20,6 +20,8 @@ import (
 	"io"
 
 	"github.com/olekukonko/tablewriter"
+	"github.com/olekukonko/tablewriter/renderer"
+	"github.com/olekukonko/tablewriter/tw"
 )
 
 // NewTableWriter creates a new table writer with the given output and options.
@@ -30,4 +32,44 @@ func NewTableWriter(output io.Writer, options ...tablewriter.Option) *tablewrite
 	}
 
 	return table
+}
+
+// NewTableWriterWithDefaults creates a new table writer with default markdown configuration.
+// It includes left alignment, markdown renderer, and custom borders optimized for terminal output.
+func NewTableWriterWithDefaults(output io.Writer, options ...tablewriter.Option) *tablewriter.Table {
+	defaultOptions := []tablewriter.Option{
+		tablewriter.WithConfig(tablewriter.Config{
+			Header: tw.CellConfig{
+				Alignment: tw.CellAlignment{Global: tw.AlignLeft},
+			},
+		}),
+		tablewriter.WithRenderer(renderer.NewMarkdown()),
+		tablewriter.WithRendition(tw.Rendition{
+			Symbols: tw.NewSymbols(tw.StyleMarkdown),
+			Borders: tw.Border{
+				Left:   tw.On,
+				Top:    tw.Off,
+				Right:  tw.On,
+				Bottom: tw.Off,
+			},
+			Settings: tw.Settings{
+				Separators: tw.Separators{
+					BetweenRows: tw.On,
+				},
+			},
+		}),
+		tablewriter.WithRowAutoWrap(tw.WrapNone),
+	}
+
+	defaultOptions = append(defaultOptions, options...)
+
+	return NewTableWriter(output, defaultOptions...)
+}
+
+// NewTableWriterWithDefaultsAndHeader creates a new table writer with default configuration and header.
+func NewTableWriterWithDefaultsAndHeader(output io.Writer, header []string, options ...tablewriter.Option) *tablewriter.Table {
+	headerOption := tablewriter.WithHeader(header)
+	allOptions := append([]tablewriter.Option{headerOption}, options...)
+
+	return NewTableWriterWithDefaults(output, allOptions...)
 }
